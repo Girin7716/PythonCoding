60fea099234fc20a0257dcb51ce62d870975d32b
Girin7716 <k3832580@naver.com>
2020-12-25 17:55:50 +0900

    readme, Q17 20201225

--- pythonBook/Problem Solving/Q17.py
+++ pythonBook/Problem Solving/Q17.py
@@ -1,1 +1,77 @@
 # 경쟁적 전염 / p344 / DFS, BFS
+
+import heapq
+
+N, K = map(int,input().split())
+board = []
+for i in range(N):
+    board.append(list(map(int,input().split())))
+S,X,Y = map(int,input().split())
+
+def get_virus(board,pq,N,pr):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] != 0:
+                heapq.heappush(pq,(pr,board[i][j],i,j))
+
+pq = []
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+time = 0
+get_virus(board,pq,N,time+1)
+while pq:
+    time,virus,x,y = heapq.heappop(pq)
+    if time == S+1:
+        break
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<N and 0<=ny<N and board[nx][ny] == 0:
+            board[nx][ny] = virus
+            heapq.heappush(pq,(time+1,board[nx][ny],nx,ny))
+
+print(board[X-1][Y-1])
+# 시간 초과
+# import heapq
+#
+# N, K = map(int,input().split())
+# board = []
+# for i in range(N):
+#     board.append(list(map(int,input().split())))
+# S,X,Y = map(int,input().split())
+#
+# def get_virus(board,pq,N):
+#     for i in range(N):
+#         for j in range(N):
+#             if board[i][j] != 0:
+#                 heapq.heappush(pq,(board[i][j],i,j))
+#
+# pq = []
+# dx = [1,0,-1,0]
+# dy = [0,1,0,-1]
+# for i in range(S):  # S초까지 반복
+#     get_virus(board,pq,N)
+#     while pq:
+#         virus,x,y = heapq.heappop(pq)
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if 0<=nx<N and 0<=ny<N and board[nx][ny] == 0:
+#                 board[nx][ny] = virus
+#
+# print(board[X-1][Y-1])
+
+
+#input
+# 3 3
+# 1 0 2
+# 0 0 0
+# 3 0 0
+# 2 3 2
+
+# 3 3
+# 1 0 2
+# 0 0 0
+# 3 0 0
+# 1 2 2
---
+++ pythonBook/Problem Solving/README.md
@@ -0,0 +1,16 @@
+# 파이썬 책 문제에 대한 풀이법
+
+## Q1
+<details>
+<summary></summary>
+</details>
+
+
+## Q17
+<details>
+<summary>경쟁적 전염</summary>
+
+- 링크 : https://www.acmicpc.net/problem/18405
+- 
+
+</details>
