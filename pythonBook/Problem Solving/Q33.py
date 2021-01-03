0101066ce30b3943357db3e2c9751f96c5c01ca0
Girin7716 <k3832580@naver.com>
2021-01-03 13:55:15 +0900

    Q33(fail)

--- pythonBook/Problem Solving/Q33.py
+++ pythonBook/Problem Solving/Q33.py
@@ -1,14 +1,26 @@
-# 퇴사 / p377 / DP
-N = int(input())
-T=[0]
-P=[0]
-for i in range(N):
-    x,y = map(int,input().split())
-    T.append(x),P.append(y)
-dp = [0]
+# 퇴사 / p377 / DP(못품)
+# 책에서 푼 방법
+n = int(input())
+t = []
+p = []
+dp = [0] * (n+1)
+max_value = 0
 
-for i in range(N):
-    
+for _ in range(n):
+    x,y = map(int,input().split())
+    t.append(x)
+    p.append(y)
+
+for i in range(n-1,-1,-1):
+    time = t[i] + i
+    # 상담이 기간 안에 끝나는 경우
+    if time<= n:
+        dp[i]=max(p[i]+dp[time],max_value)
+        max_value = dp[i]
+    else:
+        dp[i] = max_value
+
+print(max_value)
 
 # 7
 # 3 10
