8e53480b4f25cfa2ded84113e6866e0630b15665
Girin7716 <k3832580@naver.com>
2021-01-11 22:30:39 +0900

    about LIS

---
+++ BOJ/BOJ_DP1/11054.py
@@ -0,0 +1,16 @@
+# 가장 긴 바이토닉 부분 수열
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+data = map(int,input().split())
+result = []
+
+for i in range(N):
+    mid = data[i]
+    for j in range(i):
+
+        print()
+    for j in range(i+1,N):
+        print()
