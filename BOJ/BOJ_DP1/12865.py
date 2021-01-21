1603a9a3ca4b3e70587e5bdc23c0fbf1656eafcc
Girin7716 <k3832580@naver.com>
2021-01-21 22:08:57 +0900

    re

--- BOJ/BOJ_DP1/12865.py
+++ BOJ/BOJ_DP1/12865.py
@@ -1,9 +1,24 @@
 # 평범한 배낭
+
+import sys
+
+input = sys.stdin.readline
+
 N, K = map(int,input().split())
 
-things = []
+weights = [0]
+values = [0]
 for _ in range(N):
-    things.append(list(map(int,input().split())))   # weight, value
+    w, v = map(int,input().split())
+    weights.append(w)
+    values.append(v)
 
-things.sort()
+dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
 
+for i in range(1,N+1):
+    for j in range(K,0,-1):
+        if j - weights[i] >= 0:
+            dp[i][j] = max(dp[i - 1][j], values[i] + dp[i - 1][j - weights[i]])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(max(dp[N]))
