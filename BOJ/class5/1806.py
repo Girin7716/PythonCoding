83125a9c53b61b6454ca63fbd21526dcfc8b8f7a
Girin7716 <k3832580@naver.com>
2021-01-27 21:08:26 +0900

    Solve 1806

--- BOJ/class5/1806.py
+++ BOJ/class5/1806.py
@@ -1,17 +1,27 @@
 # 부분합
+import sys
+input = sys.stdin.readline
+
 N,S = map(int,input().split())
 data = list(map(int,input().split()))
 
-min_value = int(1e9)
-min_index = 0
-for size in range(N):
-    for i in range(N):
-        rem = sum(data[i:i+size])
-        if rem == S:
-            print(size)
-            exit()
-        elif rem>S and rem < min_value:
-            min_value = rem
-            min_index = size
-            break
-print(min_index)
+start = 0
+end = 0
+interval_sum = 0
+
+min_index = int(1e9)
+check = False
+for start in range(N):
+    while interval_sum < S and end < N:
+        interval_sum += data[end]
+        end += 1
+    if interval_sum >= S and min_index > (end-start):
+        min_index = end-start
+        check = True
+
+    interval_sum -= data[start]
+
+if check == True:
+    print(min_index)
+else:
+    print(0)
