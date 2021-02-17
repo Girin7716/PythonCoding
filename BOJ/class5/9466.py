a27e3f84f5f22fda4f9b85823d5d029d4490557d
Girin7716 <k3832580@naver.com>
2021-02-17 21:54:23 +0900

    Solve 9466

--- BOJ/class5/9466.py
+++ BOJ/class5/9466.py
@@ -1,24 +1,70 @@
-# 텀 프로젝트
+# # 텀 프로젝트
+import sys
+from _collections import deque
 
-def find_parent(parent,x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent,x)
-    return parent[x]
+tc = int(sys.stdin.readline())
+for _ in range(tc):
+    n = int(sys.stdin.readline())
 
-def union_parent(parent,a,b):
-    a = find_parent(parent,a)
-    b = find_parent(parent,b)
-
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-for _ in range(int(input())):
-    n = int(input())
-    student = [0] + [i for i in range(1,n+1)]
-    selected = [0] + list(map(int,input().split()))
-
-    print(n,student,selected)
+    graph = [0] + list(map(int, sys.stdin.readline().rstrip().split()))
 
 
+    indegree = [0] * (n + 1)
+    for v in graph[1:]:
+        indegree[v] += 1
+
+    q = deque()
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    visit = [False] * (n+1)
+    noCycleNode = set()
+    while q:
+        a = q.popleft()
+        noCycleNode.add(a)
+
+        indegree[graph[a]] -= 1
+        if indegree[graph[a]] == 0:
+            q.append(graph[a])
+
+    print(len(noCycleNode))
+
+# import sys
+# sys.setrecursionlimit(10**8)
+# input = sys.stdin.readline
+#
+# def dfs(node):
+#     global cnt
+#
+#     visited[node] = True
+#     next = graph[node]
+#
+#     if visited[next] == False:  # 방문 안했으면 next 방문하기
+#         dfs(next)
+#     elif fin[next] == False:    # next를 이미 방문했지만, 방문이 종료되지 않은 정점이라면 cycle
+#         temp = next
+#         while True:
+#             cnt+=1
+#             temp = graph[temp]
+#             if temp == next:
+#                 break
+#
+#     fin[node] = True
+#
+# for _ in range(int(input())):
+#     n = int(input())
+#     selected = [0] + list(map(int,input().split()))
+#     cnt = 0
+#     graph = {}
+#     for i in range(1,n+1):
+#         graph[i] = selected[i]
+#
+#     visited = [False for _ in range(n+1)]
+#     fin = [False for _ in range(n+1)]
+#
+#     for i in range(1,n+1):
+#         if visited[i] is False:
+#             dfs(i)
+#
+#     print(n-cnt)
