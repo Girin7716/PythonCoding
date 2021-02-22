b252c2630281fb4cd08844a32e4b1456f0587fa0
Girin7716 <k3832580@naver.com>
2021-02-22 17:13:36 +0900

    Solve 1516

--- BOJ/class5/1516.py
+++ BOJ/class5/1516.py
@@ -8,16 +8,19 @@ N = int(input())
 graph = [[] for _ in range(N+1)]
 weight = [[]]
 indegree = [0] * (N+1)
+reverse_graph = [[] for _ in range(N+1)]
 
 for i in range(1,N+1):
     weight.append(list(map(int,input().split())))
     for j in range(1,len(weight[i])-1):
         indegree[i] += 1
         graph[weight[i][j]].append(i)
+        reverse_graph[i].append(weight[i][j])
 
 q = deque()
 
-result = [int(1e9)] * (N+1)
+#result = [int(1e9)] * (N+1)
+result = [-1] * (N+1)
 for i in range(1,N+1):
     if indegree[i] == 0:
         q.append(i)
@@ -32,8 +35,25 @@ while q:
 
         if indegree[next] == 0:
             q.append(next)
-            result[next] = min(result[next],result[now]+weight[next][0])
+            #result[next] = min(result[next],result[now]+weight[next][0])
+            #result[next] = max(result[next], result[now] + weight[next][0])
+            for i in reverse_graph[next]:
+                result[next] = max(result[next],result[i] + weight[next][0])
 
 
 for i in result[1:]:
     print(i)
+
+# 5
+# 10 -1
+# 10 1 -1
+# 4 1 -1
+# 4 3 1 -1
+# 3 3 -1
+
+# 5
+# 10 -1
+# 10 1 4 5 -1
+# 4 1 -1
+# 4 3 1 -1
+# 3 3 -1
