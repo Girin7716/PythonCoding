1c8738cafac0a53926243ee13700458e729744da
Girin7716 <k3832580@naver.com>
2021-02-22 21:55:06 +0900

    Solve 7453

--- BOJ/class5/7453.py
+++ BOJ/class5/7453.py
@@ -1,9 +1,38 @@
 # 합이 0인 네 정수
-N = int(input())
-data = []
-for i in range(N):
-    data.append(list(map(int,input().split())))
+import sys
+input = sys.stdin.readline
 
-#data.sort(key = lambda x : x[3])
-print(data)
+n = int(input())
 
+A = []
+B = []
+C = []
+D = []
+for i in range(n):
+    a,b,c,d = map(int,input().split())
+    A.append(a)
+    B.append(b)
+    C.append(c)
+    D.append(d)
+
+ab = {}
+cd = {}
+for a in A:
+    for b in B:
+        if not ab.get(a+b):
+            ab[a+b] = 1
+        else:
+            ab[a+b] += 1
+for c in C:
+    for d in D:
+        if not cd.get(c+d):
+            cd[c+d] = 1
+        else:
+            cd[c+d] += 1
+
+result = 0
+for i in ab:
+    if cd.get(-i):
+        result += ab[i] * cd[-i]
+
+print(result)
